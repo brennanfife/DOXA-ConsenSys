401cd3f8ebb427d5e5e0e2d397fc2d9a8cba6219
Brennan Fife <brenfife@gmail.com>
2019-07-09 13:16:40 -0500

    commet commits

--- contracts/PrizedLinkedContract.sol
+++ contracts/PrizedLinkedContract.sol
@@ -20,31 +20,14 @@ contract PrizedLinkedContract {
 /* contract PrizedLinkedContract {
     // VARIABLES
     address public owner;
-    // address[] public entrants;
-    // creationTime = now;
+    address[] public entrants;
+    creationTime = now;
 
     mapping (address => uint) savings; // amount of each savings
     mapping (address => uint) entryMap; // list the number of entrants
 
-    // pool size and minimum
-    uint pool = 0;
-    uint min = 10 finney; //0.01 ether
-
-    constructor() public {
-        owner = msg.sender;
-        //creationTime = now;
-    }
-
-    //States of Contract:
-    //PoolOpen: Accepting
-    //Saving: and Earning
-    //PayOut: Withdraw
-
-    enum States {
-        PoolOpen,  //0
-        Saving,    //1
-        PayOut     //2
-    }
+    uint pool = 0; // pool size
+    uint min = 10 finney; // 0.01 ether
 
     // EVENTS
     // Event emitted when a saver dives into the pool
@@ -53,12 +36,31 @@ contract PrizedLinkedContract {
     // Event emitted when a saver withdraws
     event takeHome(address indexed saver, uint savings);
 
+    // How much someone is saving...?
     event Saving(States state);
 
+    // MODIFIERS
+    modifier isOwner {
+        require(owner == msg.sender, "Caller is not owner");
+        _;
+    }
 
-    //When a new saver joins the pool
-    //Saver can add to deposit during PoolOpen
-    function addToPool() public payable atState(States.PoolOpen) {
+    constructor() public {
+        owner = msg.sender;
+        //creationTime = now;
+    }
+
+    //States of Contract:
+        //isOpen: Accepting
+        //payOut: Withdraw
+
+    enum States {
+        isOpen,  //0
+        payOut     //1
+    }
+
+    // When a new saver joins the pool, saver can add to deposit during isOpen
+    function addToPool() public payable atState(States.isOpen) {
 
         require(msg.value >= min, "Must submit at least the minimum amount");
         addEntrant(msg.sender);
@@ -67,7 +69,7 @@ contract PrizedLinkedContract {
         emit splashDown(msg.sender, msg.value, savings[msg.sender]);
     }
 
-    //Add entrant to pool
+    // Add an entrant to the pool
     function addEntrant (address entrant) private {
         if (savings[entrant] == 0) {
           //Only add to potential winners if current balance 0
@@ -75,6 +77,13 @@ contract PrizedLinkedContract {
           entryMap[entrant] = entrants.length - 1;
         }
         //Else: Already an active key
+    }
+
+    function endPool() public isOwner {
+        myEvent.isOpen = false;
+        uint balanceTransfered = myEvent.sales * TICKET_PRICE;
+        owner.transfer(balanceTransfered);
+        emit LogEndSale(owner, balanceTransfered);
     }
 
 } */
